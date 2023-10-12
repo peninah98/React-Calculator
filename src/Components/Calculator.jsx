b057c4236a4e8319b2cb29ff46b43204bcba2e9c
peninah98 <peninangizwenayo@gmail.com>
2023-10-12 18:10:03 +0200

    Calculator updates

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,48 +1,20 @@
-import React from 'react';
-import { useState } from 'react';
+import React from 'react'
+import { useState } from 'react'
 
-const Calculator = () => {
-    const Calculator = () => {
-        const [display, setDisplay] = useState('0');
-        const [firstOperand, setFirstOperand] = useState(null);
-        const [operator, setOperator] = useState(null);
-        const [waitingForSecondOperand, setWaitingForSecondOperand] = useState(false);
+const Calculator =()=> {
+    const [data, setData] = useState("")
+    const clickHandler = (e)=>{
+        setData(data + e.target.value)
     }
-
-
-
+    const buttons = []
+    [9,8,7,6,5,4,3,2,1,".","%"].map(item =>(
+        buttons.push(
+            <button onClick={clickHandler} value={item} key={item}></button>
+        )
+    ))
   return (
-    <div className="w-64 mx-auto p-4 border border-gray-300 rounded-lg shadow-lg mt-10">
-      <div className="bg-gray-100 p-2 rounded-md">
-        <input
-          id="display"
-          className="w-full text-right text-2xl font-semibold "
-          type="text"
-       
-        />
-      </div>
-      <div className="grid grid-cols-4 gap-2 mt-4">
-        <button className="col-span-2 p-2 text-2xl bg-orange-500 rounded-md hover:bg-gray-400 hover:text-white">AC</button>
-        <button className="p-2 text-2xl  bg-orange-500 rounded-md hover:bg-gray-500 hover:text-white">+-</button>
-        <button className="p-2 text-2xl bg-orange-500 rounded-md hover:bg-gray-400 hover:text-white">%</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">7</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">8</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">9</button>
-        <button className="p-2 text-2xl bg-orange-500 rounded-md hover:bg-gray-400 hover:text-white">*</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">4</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">5</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">6</button>
-        <button className="p-2 text-2xl bg-orange-500 rounded-md hover:bg-gray-400 hover:text-white">-</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">1</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">2</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-400 hover:text-white">3</button>
-        <button className="p-2 text-2xl bg-orange-500 rounded-md hover:bg-gray-400 hover:text-white">Del</button>
-        <button className="col-span-2 p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">0</button>
-        <button className="p-2 text-2xl bg-gray-100 rounded-md hover:bg-gray-500 hover:text-white">.</button>
-        <button className="p-2 text-2xl bg-orange-500 rounded-md hover:bg-gray-400 hover:text-white">=</button>
-      </div>
-    </div>
-  );
-};
+    <div className='m-auto'>Calculator</div>
+  )
+}
 
-export default Calculator;
+export default Calculator
