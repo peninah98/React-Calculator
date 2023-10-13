dfa1fefff2b7e81482cf66864a1d07a2496a50fd
peninah98 <peninangizwenayo@gmail.com>
2023-10-13 14:58:56 +0200

    Formarted with prettier

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Calculator = () => {
-  const [data, setData] = useState(""); 
+  const [data, setData] = useState("");
 
   const buttons = [];
 
-  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0,".", "%"].forEach(element => {
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((element) => {
     buttons.push(
       <button
-        onClick={e => {
-          setData(data + e.target.getAttribute('data-value')); 
+        onClick={(e) => {
+          setData(data + e.target.getAttribute("data-value"));
         }}
-        data-value={element} 
+        data-value={element}
         key={element}
-        className='bg-yellow-600 rounded-md p-2 border text-bold text-2xl hover:text-white hover:bg-gray-700'
+        className="bg-yellow-600 rounded-md p-2 border text-bold text-2xl hover:text-white hover:bg-gray-700"
       >
         {element}
       </button>
@@ -23,77 +23,81 @@ const Calculator = () => {
   return (
     <div>
       <div>
-        <div className='m-auto  border border-black w-1/2 mt-10 mb-6 py-4 bg-orange-100 px-2 text-gray-700'>{data}</div>
-        <div className='m-auto  grid grid-cols-3 w-1/2 justify-center items-center'>
-          {buttons}
-          
+        <div className="m-auto  border border-black w-1/2 mt-10 mb-6 py-4 bg-orange-100 px-2 text-gray-700">
+          {data}
         </div>
-        
+        <div className="m-auto  grid grid-cols-3 w-1/2 justify-center items-center">
+          {buttons}
+        </div>
       </div>
-      <div className='bg-gray-600 p-2 w-1/2 m-auto flex justify-between'>
-          <button 
-              className='shadow-xl w-12 p-2 rounded-md bg-orange-200'
-              onClick={()=> setData(data.substring(0,data.length-1))}>
-            Clear
-          </button>
-          <button 
-                    className='shadow-xl w-12 p-2 rounded-md bg-orange-200'
-
-            onClick={()=>setData("")}>
-            AC
-          </button>
-          <button 
-          onClick={e=> setData(data + e.target.value)} value="+-"
-          className='shadow-xl w-12 p-2 rounded-md bg-orange-200 '
+      <div className="bg-gray-600 p-2 w-1/2 m-auto flex justify-between">
+        <button
+          className="shadow-xl w-12 p-2 rounded-md bg-orange-200"
+          onClick={() => setData(data.substring(0, data.length - 1))}
+        >
+          Clear
+        </button>
+        <button
+          className="shadow-xl w-12 p-2 rounded-md bg-orange-200"
+          onClick={() => setData("")}
+        >
+          AC
+        </button>
+        <button
+          onClick={(e) => setData(data + e.target.value)}
+          value="+-"
+          className="shadow-xl w-12 p-2 rounded-md bg-orange-200 "
         >
           +-
         </button>
-          
-        </div>
-      <div className='bg-gray-600 p-2  grid grid-cols-5 w-1/2 m-auto items-center'>
-        <button 
-          onClick={e=> setData(data + e.target.value)} value="+"
-          className='shadow-xl w-12 p-2 rounded-md bg-orange-200'
+      </div>
+      <div className="bg-gray-600 p-2  grid grid-cols-5 w-1/2 m-auto items-center">
+        <button
+          onClick={(e) => setData(data + e.target.value)}
+          value="+"
+          className="shadow-xl w-12 p-2 rounded-md bg-orange-200"
         >
           +
         </button>
-        <button 
-          onClick={e=> setData(data + e.target.value)} value="-"
-          className='shadow-xl w-12 p-2 rounded-md bg-orange-200'
-
+        <button
+          onClick={(e) => setData(data + e.target.value)}
+          value="-"
+          className="shadow-xl w-12 p-2 rounded-md bg-orange-200"
         >
           -
         </button>
-        <button 
-          onClick={e=> setData(data + e.target.value)} value="*"
-          className='shadow-xl w-12 p-2 rounded-md bg-orange-200'
-
+        <button
+          onClick={(e) => setData(data + e.target.value)}
+          value="*"
+          className="shadow-xl w-12 p-2 rounded-md bg-orange-200"
         >
           *
         </button>
-        <button 
-          onClick={e=> setData(data + e.target.value)} value="/"
-          className='shadow-xl w-12 p-2 rounded-md bg-orange-200'
+        <button
+          onClick={(e) => setData(data + e.target.value)}
+          value="/"
+          className="shadow-xl w-12 p-2 rounded-md bg-orange-200"
         >
           /
         </button>
-        <button 
-          className='shadow-xl w-12 p-2 rounded-md bg-orange-200'
+        <button
+          className="shadow-xl w-12 p-2 rounded-md bg-orange-200"
           onClick={() => {
-              try {
-                setData(
-                  String(eval(data)).length > 3 &&
+            try {
+              setData(
+                String(eval(data)).length > 3 &&
                   String(eval(data)).includes(".")
-                    ? String(eval(data).toFixed(4))
-                    : String(eval(data))
-                );
-              } catch (error) {
-                console.log(error);
-              }
-            }} value="=">
-              =
+                  ? String(eval(data).toFixed(4))
+                  : String(eval(data))
+              );
+            } catch (error) {
+              console.log(error);
+            }
+          }}
+          value="="
+        >
+          =
         </button>
-
       </div>
     </div>
   );
